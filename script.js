c4fc08230116442d187266bb15f3a735ff808d70
Bbondong <Benybadibanga13@gmail.com>
2026-05-17 22:27:31 +0100

    "ok"

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function send() {
   add("Vous", message);
   input.value = "";
 
-  const res = await fetch("/api/chat", {
+  const res = await fetch("/api/app", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message })
